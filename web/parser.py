f94257db60be1d80b70d33616f68746e115116f1
ieiayaobb <119133894>
2016-05-23 00:43:40 +0800

    remove unused cookie

--- web/parser.py
+++ web/parser.py
@@ -25,7 +25,7 @@ def parse_taobao(shop):
         'Host': host,
         'Referer': referer,
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': 'cna=ObCnD/d8VXECAXBRWEadZOZM; tk_trace=1; swfstore=169732; uc3=nk2=CsOUZBCP6AL5&id2=VWeQ%2FCNIuv0%3D&vt3=F8dASmrHVtXjkMGHq4k%3D&lg2=VT5L2FSpMGV7TQ%3D%3D; uss=UoTcCaHtRRtGcFeWMY9Wo%2BILZ8Ql7iEOVGdnS4VCgTi7uXgb8PD1suzdHfM%3D; lgc=ieiayaobb; tracknick=ieiayaobb; cookie2=9cd772dd187e165d8d9cbba701b935b5; skt=0ee787eff6410466; t=65af5e3ef1b39313d58971b6b638418f; _tb_token_=cYDIh0Iv0L88; whl=-1%260%260%260; x=__ll%3D-1%26_ato%3D0; otherx=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0; pnm_cku822=; cq=ccp%3D1; l=AszMn7k9t3HDODJJ/qVfpqvZHCD-BXCv',
+        'Cookie': 'cookie2=9cd772dd187e165d8d9cbba701b935b5; t=65af5e3ef1b39313d58971b6b638418f; _tb_token_=cYDIh0Iv0L88;',
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36',
     }
 
