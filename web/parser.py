f480a21a3c4677554a7e3089b66d74c03fca1e29
shenyineng <shenyineng@patsnap.com>
2016-05-26 13:30:44 +0800

    fix cookie issues

--- web/parser.py
+++ web/parser.py
@@ -2,43 +2,21 @@
 # -*- coding: utf-8 -*-
 import re
 import httplib2 as httplib2
+import requests
 
 from web.models import Good
-
+http = httplib2.Http()
 
 def parse_taobao(shop):
-    url = re.search('https://.*\..*\.com', shop.url).group()
-    host = url[8:]
-    referer = 'http://' + host
-    print host
-    print referer
-    http = httplib2.Http()
-    http.follow_all_redirects = False
-
-    headers_template = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Host': host,
-        'Referer': referer,
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': 'cookie2=9cd772dd187e165d8d9cbba701b935b5; t=65af5e3ef1b39313d58971b6b638418f; _tb_token_=cYDIh0Iv0L88;',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36',
-    }
-
-    headers = headers_template.copy()
-    resp, content = http.request(shop.url, 'GET', headers=headers)
-
-    if resp['status'] == '302':
-        headers = headers_template.copy()
-        headers['Cookie'] = resp['set-cookie']
-        resp, content = http.request(resp['location'], headers=headers)
+    shop_index_url = re.search('https://.*\..*\.com', shop.url).group()
+    session = requests.Session()
+    session.headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36'}
+    session.get(shop_index_url)
+    r = session.get(shop.url)
 
     goods = []
-    for each_content in re.finditer('<a [^>/]*?class=\\\\"J_TGoldData\\\\".*?<\/a>', content):
+    for each_content in re.finditer('<a [^>/]*?class=\\\\"J_TGoldData\\\\".*?<\/a>', r.text):
         good = Good()
         good.url = re.search('href=\\\\".*?\\\\"', each_content.group()).group().lstrip('href=\\"//').rstrip('\\"')
         good.img = re.search('lazyload=\\\\".*?\\\\"', each_content.group()).group().lstrip('lazyload=\\"//').rstrip('\\"')
