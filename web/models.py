69d3b71185decc70aaddaad0a7312bf5bc5bedaf
shenyineng <shenyineng@patsnap.com>
2016-05-27 15:00:33 +0800

    update paginator & add tag model

--- web/models.py
+++ web/models.py
@@ -2,9 +2,14 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+
 class Shop(models.Model):
     name = models.CharField(max_length=64, default='')
     url = models.CharField(max_length=2048)
+    tags = models.ManyToManyField(Tag)
 
     def __unicode__(self):
         return self.name
@@ -14,7 +19,6 @@ class Good():
     shop = models.ForeignKey(Shop)
     img = models.CharField(max_length=2048)
     url = models.CharField(max_length=2048)
-
 
 
 class Comment(models.Model):
